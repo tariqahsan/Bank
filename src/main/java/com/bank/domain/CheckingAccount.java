9d4cb3670904f3ccf792e7b3100ea25376d484c4
Tariq Ahsan <tariqahsan@yahoo.com>
2017-10-20 12:38:59 -0400

    First commit

---
+++ src/main/java/com/bank/domain/CheckingAccount.java
@@ -0,0 +1,65 @@
+package com.bank.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class CheckingAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+    @OneToMany(mappedBy = "checkingAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<CheckingTransaction> checkingTransactionList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<CheckingTransaction> getCheckingTransactionList() {
+        return checkingTransactionList;
+    }
+
+    public void setCheckingTransactionList(List<CheckingTransaction> checkingTransactionList) {
+        this.checkingTransactionList = checkingTransactionList;
+    }
+
+
+}
+
+
+
